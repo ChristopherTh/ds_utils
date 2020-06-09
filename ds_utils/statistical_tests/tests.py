dcd3c3ad6e9681e254f1444ebcbc3b6b00c24351
Christopher Thiemann <christopherth94@googlemail.com>
2020-06-09 15:05:43 +0200

    Added space for statistical testing.

---
+++ ds_utils/statistical_tests/tests.py
@@ -0,0 +1,22 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import scipy.stats
+
+def t_test(data: pd.Series, hypo: int):
+	"Performs simple t_test"""
+	
+	return p_val, t_statistic
+	
+def rank_test():
+
+	return p_val, t_statistic
+	
+def signed_rank_test():
+
+	return p_val
+	
+
+
