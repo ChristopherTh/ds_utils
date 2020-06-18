f96f81525b5fff1a974611edf584b703c3d449a2
Christopher Thiemann <christopherth94@googlemail.com>
2020-06-18 13:29:15 +0200

    Add placeholder for univariate time series plots.

---
+++ ds_utils/timeseries/time_series.py
@@ -0,0 +1,35 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import scipy.stats
+import statsmodel.api as sm
+
+def pac():
+
+	return
+	
+def acu():
+
+	return
+	
+def polar_plot():
+	return
+	
+def season_plot():
+
+	return
+	
+def lineplot():
+
+	return
+	
+
+
+if __name__ == 'main':
+
+	pd.
+	
+
+
