452ed964af167dcbc1777d869fb318f584ec6d83
Christopher Thiemann <christopherth94@googlemail.com>
2020-06-20 15:08:44 +0200

    Made code more general.

--- ds_utils/timeseries/time_series.py
+++ ds_utils/timeseries/time_series.py
@@ -8,7 +8,7 @@ import matplotlib.dates as mdates
 
 
 class ts_eda:
-    def __init__(self, ts, freq="d"):
+    def __init__(self, ts, freq="d", lags = 9):
 
         self.ts = ts
         self.cwd = Path.cwd()
@@ -17,7 +17,7 @@ class ts_eda:
             self.plot_folder.mkdir()
 
         self.ts_df = self.make_df()
-
+		self.lags = lags
         self.create_lags(self.ts_df, 9)
 
         # make quick plot to infer additive or multiplicative seasonality
@@ -48,7 +48,7 @@ class ts_eda:
             7: "Sunday",
         }
 
-        ts_df = pd.DataFrame(self.ts)
+        ts_df = pd.DataFrame(self.ts, columns = 'y')
         ts_df["month"] = self.ts.index.month
         ts_df.month = ts_df.month.map(month_map)
         ts_df["year"] = self.ts.index.year
@@ -57,14 +57,14 @@ class ts_eda:
 
         return ts_df
 
-    def create_lags(self, df: pd.DataFrame, lags=1, fill_value=np.nan):
-        for i in np.arange(1, lags + 1):
-            df[f"lag_{i }"] = np.append(df[0][(i):].values, [fill_value] * (i))
+    def create_lags(self, df: pd.DataFrame, fill_value=np.nan):
+        for i in np.arange(1, self.lags + 1):
+            df[f"lag_{i }"] = np.append(df['y'][(i):].values, [fill_value] * (i))
 
     def timeplot(self):
         fig, axs = plt.subplots()
 
-        axs = sns.lineplot(x=self.ts.index, y=self.ts)
+        sns.lineplot(x=self.ts.index, y=self.ts, ax = axs)
         fig.autofmt_xdate()
 
         plt.savefig(self.plot_folder / "timeplot.png")
@@ -73,7 +73,7 @@ class ts_eda:
     def season_plot(self,):
         fig, axs = plt.subplots()
 
-        sns.lineplot(x="month", y=0, hue="year", data=self.ts_df, ci=False, ax=axs)
+        sns.lineplot(x="month", y='y', hue="year", data=self.ts_df, ci=False, ax=axs)
         plt.savefig(self.plot_folder / "season1.png")
         plt.clf()
 
@@ -82,20 +82,20 @@ class ts_eda:
             plt.hlines(x.mean(), self.ts_df.year.min(), self.ts_df.year.max())
 
         g = sns.FacetGrid(self.ts_df, col="month", sharex=True, sharey=False)
-        g.map(sns.lineplot, "year", 0, ci=False)
-        g.map(plot_mena, 0)
+        g.map(sns.lineplot, "year", 'y', ci=False)
+        g.map(plot_mena, 'y')
         plt.savefig(self.plot_folder / "season2.png")
         plt.clf()
 
     def acf(self,):
         fig, axes = plt.subplots()
-        sm.graphics.tsa.plot_acf(self.ts_df[0], lags=40, ax=axes)
+        sm.graphics.tsa.plot_acf(self.ts_df['y'], lags=40, ax=axes)
         plt.savefig(self.plot_folder / "auto_correlation_function.png")
         plt.clf()
 
     def pacf(self,):
         fig, axes = plt.subplots()
-        sm.graphics.tsa.plot_pacf(self.ts_df[0], lags=40, ax=axes)
+        sm.graphics.tsa.plot_pacf(self.ts_df['y'], lags=40, ax=axes)
         plt.savefig(self.plot_folder / "partial_auto_correlation_function.png")
         plt.clf()
 
@@ -129,7 +129,7 @@ class ts_eda:
                 self.ts_df[self.ts_df.index.year == year].index.min().to_pydatetime(),
                 self.ts_df[self.ts_df.index.year == year].index.max().to_pydatetime(),
             )
-            rand_nums = self.ts_df[self.ts_df.index.year == year][0].values
+            rand_nums = self.ts_df[self.ts_df.index.year == year]['y'].values
             df = pd.DataFrame(index=times, data=rand_nums, columns=["A"])
             t = mdates.date2num(df.index.to_pydatetime())
             y = df["A"]
@@ -140,14 +140,18 @@ class ts_eda:
         plt.clf()
 
     def lag_plot(self,):
-        fig, axs = plt.subplots(3, 3, figsize=(10, 10), tight_layout=True)
+
+		if self.lags % 2 == 0:
+        	fig, axs = plt.subplots(self.lags / 2, 2, figsize=(10, 10), tight_layout=True)
+		else:
+			fig, axs = plt.subplots((self.lags + 1) / 2, 2, figsize=(10, 10), tight_layout=True)
+
         for i, axis in enumerate(axs.flat):
             sns.scatterplot(
-                x=f"lag_{i + 1 }", y=0, data=self.ts_df, ax=axis, hue="month"
+                x=f"lag_{i + 1 }", y='y', data=self.ts_df, ax=axis, hue="month"
             )
             handles, labels = axis.get_legend_handles_labels()
             fig.legend(handles, labels, loc="right")
             axis.get_legend().remove()
         plt.savefig(self.plot_folder / "lag_plot.png")
         plt.clf()
-
